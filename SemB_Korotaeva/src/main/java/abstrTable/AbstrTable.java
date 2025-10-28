65198efbee360aa097d9830c3ba00cd97d4d2d2b
Angelina Korotaeva <st69617@upce.cz>
2025-10-28 16:16:02 +0100

    Commit

--- SemB_Korotaeva/src/main/java/abstrTable/AbstrTable.java
+++ SemB_Korotaeva/src/main/java/abstrTable/AbstrTable.java
@@ -21,10 +21,10 @@ public class AbstrTable<K extends Comparable<K>, V> implements IAbstrTable<K, V>
         Node<K, V> node = root;
 
         while (node != null) {
-            int cislo = key.compareTo(node.key);
-            if (cislo > 0) {
+            int keyCislo = key.compareTo(node.key);
+            if (keyCislo > 0) {
                 node = node.right;
-            } else if (cislo < 0) {
+            } else if (keyCislo < 0) {
                 node = node.left;
             } else {
                 return node.value;
@@ -39,30 +39,49 @@ public class AbstrTable<K extends Comparable<K>, V> implements IAbstrTable<K, V>
         valueNeniNull(value);
 
         Node<K, V> node = new Node<>(key, value);
-
+        boolean NovyPrvekUzExistue = false;
         if (jePrazdny()) {
             root = node;
         } else {
             Node<K, V> current = root;
             while (current != null) {
-                int cislo = key.compareTo(current.key);
-                if (cislo > 0) {
+                int keyCislo = key.compareTo(current.key);
+                if (keyCislo > 0) {
                     if (current.right == null) {
                         current.right = node;
                         break;
-                    } else {
-                        current = current.right;
                     }
-                } else if (cislo < 0) {
+                    current = current.right;
+                } else if (keyCislo < 0) {
                     if (current.left == null) {
                         current.left = node;
                         break;
-                    } else {
-                        current = current.left;
-                    }
+                    } 
+                    current = current.left;
                 } else {
                     current.value = value;
+                    NovyPrvekUzExistue = true;
                     break;
+                }
+            }
+            if (!NovyPrvekUzExistue) {
+                current = root;
+                while (current != null) {
+                    int keyCislo = key.compareTo(current.key);
+                    current.pocetPotomku++;
+                    if (keyCislo > 0) {
+                        if (current.right == null) {
+                            break;
+                        } 
+                        current = current.right;
+                    } else if (keyCislo < 0) {
+                        if (current.left == null) {
+                            break;
+                        } 
+                        current = current.left;
+                    } else {
+                        break;
+                    }
                 }
             }
         }
@@ -73,7 +92,57 @@ public class AbstrTable<K extends Comparable<K>, V> implements IAbstrTable<K, V>
         prazdnySeznam();
         keyNeniNull(key);
 
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Node<K, V> odebranyPrvek = root;
+
+        if (root.right == null && root.left == null) {
+            zrus();
+        }
+
+        Node<K, V> current = root;
+        Node<K, V> parentCurrent = root;
+        while (current != null) {
+            int keyCislo = key.compareTo(current.key);
+            if (keyCislo > 0) {
+                parentCurrent = current;
+                current = current.right;
+            } else if (keyCislo < 0) {
+                parentCurrent = current;
+                current = current.left;
+            } else {
+
+                if (current.right == null && current.left == null) {
+                    if (parentCurrent.right == current) {
+                        parentCurrent.right = null;
+                        return current.value;
+                    } else {
+                        parentCurrent.left = null;
+                        return current.value;
+                    }
+                } else {
+                    boolean parentMaChild = true;
+                    if (current.right != null && current.left != null) {
+
+                        while (parentMaChild) {
+                            K keyCurrent = current.key;
+
+                            if (current.right == null) {
+
+                            }
+                        }
+                    } else {
+                        while (parentMaChild) {
+                            K keyCurrent = current.key;
+
+                            if (current.right == null) {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return odebranyPrvek.value;
     }
 
     @Override
@@ -111,7 +180,7 @@ public class AbstrTable<K extends Comparable<K>, V> implements IAbstrTable<K, V>
     @Override
     public int rank(K key) {
         int rank = 0;
-        
+
         AbstrLIFO<Node<K, V>> lifo = new AbstrLIFO<>();
         Node<K, V> current = root;
         while (current != null || !lifo.jePrazdny()) {
@@ -120,28 +189,45 @@ public class AbstrTable<K extends Comparable<K>, V> implements IAbstrTable<K, V>
                 current = current.left;
             }
             Node<K, V> data = lifo.odeber();
-            
+            rank++;
+            if (key == data.key) {
+                return rank;
+            }
             current = data.right;
         }
-        
-        return rank;
+
+        return 0;
     }
 
     @Override
     public Iterator iterator(ETypProhl typ) {
         switch (typ) {
             case SIRKA -> {
-                return new Iterator() {
+                AbstrFIFO<Node<K, V>> abstrFIFO = new AbstrFIFO<>();
+                if (root != null) {
+                    abstrFIFO.vloz(root);
+                }
+                return new Iterator<V>() {
                     @Override
                     public boolean hasNext() {
-                        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+                        return !abstrFIFO.jePrazdny();
                     }
 
                     @Override
-                    public Object next() {
-                        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+                    public V next() {
+                        if (!hasNext()) {
+                            throw new NoSuchElementException();
+                        } else {
+                            Node<K, V> data = abstrFIFO.odeber();
+                            if (data.left != null) {
+                                abstrFIFO.vloz(data.left);
+                            }
+                            if (data.right != null) {
+                                abstrFIFO.vloz(data.right);
+                            }
+                            return data.value;
+                        }
                     }
-
                 };
             }
             case HLOUBKA -> {
@@ -161,7 +247,7 @@ public class AbstrTable<K extends Comparable<K>, V> implements IAbstrTable<K, V>
                             node = node.left;
                         }
                         if (!hasNext()) {
-                            throw new NoSuchElementException("Žádné další prvky.");
+                            throw new NoSuchElementException();
                         }
                         Node<K, V> data = lifo.odeber();
                         node = data.right;
@@ -171,7 +257,7 @@ public class AbstrTable<K extends Comparable<K>, V> implements IAbstrTable<K, V>
                 };
             }
             default ->
-                throw new IllegalArgumentException("Neznámý typ prohlížení: " + typ);
+                throw new IllegalArgumentException();
         }
     }
 
@@ -199,12 +285,14 @@ public class AbstrTable<K extends Comparable<K>, V> implements IAbstrTable<K, V>
         private V value;
         private Node<K, V> right;
         private Node<K, V> left;
+        private int pocetPotomku;
 
         public Node(K key, V value) {
             this.key = key;
             this.value = value;
             right = null;
             left = null;
+            pocetPotomku = 1;
         }
 
         public Node() {
@@ -212,6 +300,7 @@ public class AbstrTable<K extends Comparable<K>, V> implements IAbstrTable<K, V>
             this.value = null;
             right = null;
             left = null;
+            pocetPotomku = 1;
         }
     }
 }
