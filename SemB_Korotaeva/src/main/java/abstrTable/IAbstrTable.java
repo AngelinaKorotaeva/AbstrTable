f755c24d16579c92ce791d62d83ced6254282e0b
Angelina Korotaeva <st69617@upce.cz>
2025-10-28 16:16:25 +0100

    Commit

--- SemB_Korotaeva/src/main/java/abstrTable/IAbstrTable.java
+++ SemB_Korotaeva/src/main/java/abstrTable/IAbstrTable.java
@@ -7,21 +7,24 @@ public interface IAbstrTable <K extends Comparable<K>, V>{
     /**
      * Vyhledá prvek dle klíče.
      * @param key
-     * @return 
+     * @return value hledaciho prvka nebo null poku prvek neni ve stromu.
+     * @throws NullPointerException pokud strom je prazdny nebo key je null.
      */
     V najdi(K key);
     
     /**
-     * 
+     * Vloží prvek do tabulky.
      * @param key
      * @param value 
+     * @throws NullPointerException pokud key nebo value je null.
      */
     void vloz(K key, V value);
     
     /**
-     * 
+     * Odebere prvek dle klíče z tabulky.
      * @param key
-     * @return 
+     * @return odebrany prvek nebo null pokud takový key neexistuje ve stromu.
+     * @throws NullPointerException pokud strom je prazdny nebo key je null.
      */
     V odeber(K key);
     
@@ -37,23 +40,25 @@ public interface IAbstrTable <K extends Comparable<K>, V>{
     boolean jePrazdny();
     
     /**
-     * 
+     * Vrátí prvek s k-tým nejmenším klíčem ve stromu.
      * @param k
-     * @return 
+     * @return value prvka
+     * @throws NullPointerException pokud strom je prazdny.
      */
     V select(int k);
     
     /**
-     * 
+     * Vrátí pořadí prvku s klíčem key.
      * @param key
-     * @return 
+     * @return pořadí prvku
+     * @throws NullPointerException pokud strom je prazdny nebo key je null.
      */
     int rank(K key);
     
     /**
-     * 
+     * Vytvoří iterátor, který umožňuje procházení stromu do šířky a hloubky.
      * @param typ
-     * @return 
+     * @return iterator podle typu.
      */
     Iterator iterator(ETypProhl typ);
 }
